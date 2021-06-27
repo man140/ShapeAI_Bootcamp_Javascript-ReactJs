0a2b353e190b1f6f893c89a4e006838cf4466d80
man140 <68772317+man140@users.noreply.github.com>
2021-06-27 16:03:23 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div class="note">
+      <h1>JavaScript</h1>
+      <p>A basic Webdev Bootcamp By Manav Ranawat</p>
+    </div>
+  );
+}
+
+export default Note;
